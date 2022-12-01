876670344209f93d48e2de6b479e568f53f6b023
Cataclock <Stevenson2628@gmail.com>
2022-12-01 15:19:18 -0800

    Game Over Screen CDG

--- Chrome Dinosaur Game/Scripts/main_scene.gd
+++ Chrome Dinosaur Game/Scripts/main_scene.gd
@@ -5,7 +5,9 @@ extends Node2D
 # How fast the objects move
 @export var BaseObjectSpeedSpeed = 200
 
-func _process(delta):
+var ShitCode = false
+
+func _process(_delta):
 	# Play a animation that looks like the ground is moving.
 	var AnimationThing = get_node("Platform/AnimationPlayer")
 	if !AnimationThing.is_playing():
@@ -13,7 +15,11 @@ func _process(delta):
 	
 	# Set the point value
 	var Spawner = get_node("Spawner")
-	get_node("Points").set("text", "Score: " + str(int(Spawner.get("Points"))))
+	
+	# Easy variable to get points
+	var GottenPoints = str(int(Spawner.get("Points")))
+	
+	get_node("Points").set("text", "Score: " + GottenPoints)
 	# Inefficiant way of speeding up the game. I don't care it is only a prototype.
 	if Spawner.get("Points") >= 500:
 		BaseObjectSpeedSpeed = 1200
@@ -30,3 +36,11 @@ func _process(delta):
 	elif Spawner.get("Points") >= 100:
 		BaseObjectSpeedSpeed = 400
 		AnimationThing.set("playback_speed", BaseAnimationSpeed * 2)
+	
+	if get_node_or_null("Dinosaur") == null && ShitCode == false:
+		get_node("Game Over!").set("visible", true)
+		get_node("Game Over!/Label").set("text", "GAME OVER!\nScore: " + GottenPoints)
+		ShitCode = true
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
